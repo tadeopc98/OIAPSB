11aadf0ac23ed43e7850ccb5a516ecaa8f8293b9
tadeopc98 <tadeoperezcoria98qgmail.com>
2024-10-11 21:03:06 -0600

    TPC cambios version 1.0

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,10 +7,11 @@ import logo from '../assets/Logo.jpg';  // Importa el logo
 
 const Navbar = () => {
   return (
+    
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src={logo} alt="Logo"/> {/* Agrega la imagen del logo */}
+          {/* <img src={logo} alt="Logo"/> Agrega la imagen del logo */}
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import './Home.css'
-import estatuto from '../assets/estatutos.webp';
+import './Home.css';
+import estatuto from '../assets/estatutos.png';
+import ef from '../assets/ef.png'
+import asm from '../assets/asamblea.png'
+import obras from '../assets/obras.png'
+import predios from '../assets/predios.png'
+import comprasPublicas from '../assets/compraspublicas.png'
+import prmociones from '../assets/promociones.png'
+import encurso from '../assets/en-curso.png'
+import maquinaria from '../assets/excavador.png'
+import servicios from '../assets/servicio.png'
 
 const Home = () => {
   const [message, setMessage] = useState('');
@@ -18,47 +27,142 @@ const Home = () => {
           {/* Tarjetas dentro del div 11 */}
           <div className="row">
             {/* Primera tarjeta */}
-            <div className="col-12 col-md-4 mb-4">
+            <div className="div_principal col-12 col-md-3 mb-3">
               <div className="card-item card">
                 <img 
                   src={estatuto} 
                   alt="Estatutos" 
                   className="card-img-top" 
-                  
                 />
                 <div className="card-body">
                   <h5 className="card-title">Estatutos</h5>
-                  <button className="btn btn-primary">Ver más ...</button>
+                  
                 </div>
               </div>
             </div>
             {/* Segunda tarjeta */}
-            <div className="col-12 col-md-4 mb-4">
+            <div className="div_principal col-12 col-md-3 mb-3">
               <div className="card-item card">
                 <img 
-                  src="/path-to-image/estatutos-image.webp" 
+                  src={ef} 
                   alt="Estatutos" 
                   className="card-img-top" 
-                  style={{ maxHeight: '200px', objectFit: 'cover' }} 
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Estatutos</h5>
-                  <button className="btn btn-primary">Ver más ...</button>
+                  <h5 className="card-title">Estados Financieros</h5>
+                  
                 </div>
               </div>
             </div>
             {/* Tercera tarjeta */}
-            <div className="col-12 col-md-4 mb-4">
+            <div className="div_principal col-12 col-md-3 mb-3">
               <div className="card-item card">
                 <img 
-                  src="/path-to-image/estatutos-image.webp" 
+                  src={asm} 
                   alt="Estatutos" 
                   className="card-img-top" 
-                  style={{ maxHeight: '200px', objectFit: 'cover' }} 
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Estatutos</h5>
-                  <button className="btn btn-primary">Ver más ...</button>
+                  <h5 className="card-title">Asambleas</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Cuarta tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={obras} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Obras</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Quinta tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={predios} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Predios</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Sexta tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={comprasPublicas} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Compras Públicas</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Septima tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={prmociones} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Promociones y descuentos</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Octava tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={encurso} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Proyectos en curso</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Novena tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={maquinaria} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Maquinaria y transporte</h5>
+                  
+                </div>
+              </div>
+            </div>
+            {/* Decima tarjeta */}
+            <div className="div_principal col-12 col-md-3 mb-3">
+              <div className="card-item card">
+                <img 
+                  src={servicios} 
+                  alt="Estatutos" 
+                  className="card-img-top" 
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Servicios</h5>
+                  
                 </div>
               </div>
             </div>
