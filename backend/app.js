322ecbcc81221df9d431795a239249231101deb3
tadeopc98 <tadeoperezcoria98qgmail.com>
2024-10-17 12:00:25 -0600

    TPC MVC

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
+const fileRoutes = require('./routes/fileRoutes');
 const app = express();
 
 app.use(cors()); // Permitir peticiones del frontend
@@ -11,7 +13,11 @@ app.get('/', (req, res) => {
     res.send('Servidor Node.js funcionando');
 });
 
+// Middleware para servir archivos estáticos (los PDFs)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', apiRoutes); // Usa las rutas de la API
+app.use('/api', fileRoutes);
 
 // Servidor corriendo en el puerto 3001
 app.listen(3001, () => {
---
+++ backend/models/files.js
@@ -0,0 +1,19 @@
+// backend/models/files.js
+const files = [
+    {
+      id: 1,
+      titulo: "Manual del Usuario",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: "/pdfs/manual_usuario.pdf"
+    },
+    {
+      id: 2,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "/pdfs/guia_rapida.pdf"
+    },
+    // Puedes agregar más archivos aquí
+  ];
+  
+  module.exports = files;
+  
