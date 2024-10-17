7266aa2b2c89bde4dbab0f6e118eb3cce3b30ed0
Iván Almaraz <ivanalmaraz@MacBook-Air-de-Ivan.local>
2024-10-17 12:11:28 -0600

    IAG lista pdf

--- backend/models/files.js
+++ backend/models/files.js
@@ -10,8 +10,117 @@ const files = [
       id: 2,
       titulo: "Guía Rápida",
       descripcion: "Guía rápida del sistema.",
-      ubicacion: "/pdfs/guia_rapida.pdf"
+      ubicacion: "../public/PDF1.pdf"
     },
+    {
+      id: 3,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 4,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 5,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 6,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 7,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 8,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 9,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 10,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 11,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 12,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 13,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 14,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 15,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 16,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 17,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 18,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 19,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+    {
+      id: 20,
+      titulo: "Guía Rápida",
+      descripcion: "Guía rápida del sistema.",
+      ubicacion: "../public/PDF1.pdf"
+    },
+
     // Puedes agregar más archivos aquí
   ];
   
