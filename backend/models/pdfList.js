67f8a79afe1c52fc284186201571ac9cdd237272
Iván Almaraz <ivanalmaraz@MacBook-Air-de-Ivan.local>
2024-11-04 11:17:18 -0600

    IAG modificacionPDFlist

--- backend/models/pdfList.js
+++ backend/models/pdfList.js
@@ -4,123 +4,161 @@ const pdfList = [
       id: 1,
       titulo: "actualizacion de software",
       descripcion: "Este es el manual de usuario para el sistema.",
-      ubicacion: "pdf1.pdf",
+      ubicacion: 
       fecha: "2024-10-31s",
       proyecto: "Comercializadora"
     },
     {
       id: 2,
-      titulo: "Recibo de luz",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "pdf2.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 3,
-      titulo: "Cotizacion de proyecto",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "pdf3.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 4,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 5,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 6,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 7,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 8,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 9,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 10,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 11,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 12,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 13,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 14,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 15,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 16,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "pdf2.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion:
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 17,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 18,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 19,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
     {
       id: 20,
-      titulo: "Guía Rápida",
-      descripcion: "Guía rápida del sistema.",
-      ubicacion: "../public/PDF1.pdf"
+      titulo: "actualizacion de software",
+      descripcion: "Este es el manual de usuario para el sistema.",
+      ubicacion: 
+      fecha: "2024-10-31s",
+      proyecto: "Comercializadora"
     },
 
     // Puedes agregar más archivos aquí
